6fdec67c0d450dfea7a8c1ec32d8a28a06ea42c3
jonathanVisbecq <j.visbecq@orange.fr>
2015-01-15 16:15:33 +0100

    Code clean up

--- dataloader.py
+++ dataloader.py
@@ -20,76 +20,99 @@ Load only symbols for which we have everything (attributes, prices,...) so as to
 
 
 class DataLoader: 
-    # Using Yahoo Finance as the stocks data provider
-    DATA_SOURCE = "yahoo"
-    # Starting date for historical prices
-    START_DATE = datetime.datetime(1990, 1, 1)
-    # Last date for historical prices
-    END_DATE = datetime.datetime(2014, 10, 30)
     # Path for saving the .csv files
     PATH_PREFIX = "/home/jonathan/Programmation/Python/Projet_statsFinance/Stocks data/"
     
-    @staticmethod
-    def predefined_index_components_path(index_name):
-        """        
-        Predefined helpful paths
-        """
-        return DataLoader.PATH_PREFIX + "Lists of symbols/" + index_name
+    # Starting date for historical prices
+    START_DATE = datetime.datetime(1990, 1, 1)
+    # Last date for historical prices
+    END_DATE = datetime.datetime(2014, 12, 31)
     
-
+    
+    
+    ###########################################################################
+    # Functions to get stock data from the internet
+    ###########################################################################
+    
     @staticmethod
-    def symbols():
+    def fetch_stock_data(symbol,erase=False,prices=True,attributes=True):
         """
-        Return the list of symbols already retrieved
+        Fetch prices time series and/or attributes for a stock (or anything 
+        with a symbol on yahoo finance)
         """
+        # Look for the name among the symbols already fetched and does nothing
+        # if already retrieved
+        symbols = DataLoader.symbols();
         
-        if os.path.isfile(DataLoader.PATH_PREFIX+"Symbols"):
-            symbols = pickle.load(open(DataLoader.PATH_PREFIX+"Symbols","r"))
-        else:
-            symbols = []  
+        if not erase:
+            if symbol in symbols:
+                return
+      
+        # If new symbol:
+        
+        if prices:
+            # Fetch and save historical prices
+            DataLoader.fetch_historical_prices(symbol,save=True)
+        
+        if attributes:
+            # Fetch attributes and save it in the Attributes folder
+            DataLoader.fetch_attributes(symbol,save=True)
+        
+        # Add the symbol to the list in the Symbols file
+        if prices or attributes:
+            DataLoader.save_symbol(symbols=symbols,symbol=symbol)
             
-        return symbols
-         
+    #--------------------------------------------------------------------------
+            
     @staticmethod
-    def read_symbols_from_file(path):
+    def fetch_listOf_stocks(list_of_symbols,erase=False,prices=True,attributes=True):
         """
-        Read symbols stored in a file such that there is one symbole per line
-        """        
+        Fetch stocks with symbols specified in 'list_of_symbols'
+        """
+        # To store the symbols we failed to retrieve
+        errors_list = {}
         
-        symbols = []
-        
-        with open(path,"r") as sym_file:
-            for symbol in iter(sym_file):
-                symbols.append(symbol[:-1]) # Last character in '\n'
+        for symbol in list_of_symbols:
+            print(symbol)
+            
+            try:
+                DataLoader.fetch_stock_data(symbol,erase=erase,prices=prices,attributes=attributes)
+                print("--Successfully saved")
+            except Exception as e:
+                print("-------------------------------")
+                print("--Error occured:" + e.message)
+                print("-------------------------------")                    
                 
-        return symbols
+                errors_list[symbol] = e.message
+                
+        return errors_list
         
-        
+    #--------------------------------------------------------------------------
+   
     @staticmethod
-    def get_historical_prices(symbol,save=False):
+    def fetch_historical_prices(symbol,save=False):
         """
         Get historical adjusted close prices from the data provider and save it
         """
         
-        ts = webData.DataReader(symbol, DataLoader.DATA_SOURCE, DataLoader.START_DATE, DataLoader.END_DATE)
-        ts = ts['Adj Close']
-        ts.name = "Price"
+        prices = webData.DataReader(symbol, "yahoo", DataLoader.START_DATE, DataLoader.END_DATE)
+        prices = prices['Adj Close']
+        prices.name = "Price"
         
-        if save:
-             # Save prices in the Prices folder
-            path_price =  DataLoader.PATH_PREFIX + "Prices/" + symbol + ".csv"
-            ts.to_csv(path=path_price,header=True)    
+        if save: # Save prices in the Prices folder
+            DataLoader.save_historical_prices(symbol,prices)
         else:
-            return ts
-            
-            
+            return prices
+
+    #--------------------------------------------------------------------------
+
     @staticmethod
-    def get_attributes(symbol,save=False):
+    def fetch_attributes(symbol,save=False):
         """
         Fetch stock's attributes from the web and save it
         """
         
-        # Fetch attributes and save it in the Attributes folder
+        # Fetch attributes and save it in the 'Attributes' folder
         stock_attributes = StockAttributes.get_stock_attributes(symbol)
         
         if save:
@@ -98,22 +121,64 @@ class DataLoader:
             return stock_attributes
             
             
+    ###########################################################################
+    # Functions to fetch interest rates data from the internet
+    ###########################################################################
+    
+    @staticmethod
+    def fetch_historical_interest_rates(name,save=False):
+        """
+        """
+        symbols = {"LIBOR ON USD":"USDONTD156N",
+                   "LIBOR ON EUR":"EURONTD156N",
+                   "LIBOR ON GBP":"GBPONTD156N",
+                   "FED FUNDS RATES":"DFF"}        
+        
+        rates = webData.DataReader(symbols[name], "fred", DataLoader.START_DATE, DataLoader.END_DATE)
+        rates.name = name
+        
+        if save:
+            DataLoader.save_historical_interest_rates(name,rates)
+        else:
+            return rates
+
+    ###########################################################################
+    # Functions to save data
+    ###########################################################################
+
+    @staticmethod
+    def save_historical_prices(symbol,prices):
+        """
+        """
+        path_price =  DataLoader.PATH_PREFIX + "Prices/" + symbol + ".csv"
+        prices.to_csv(path_or_buf=path_price,header=True)    
+
+    #--------------------------------------------------------------------------
+
+    @staticmethod
+    def save_historical_interest_rates(name,rates):
+        """
+        """
+        path_rates =  DataLoader.PATH_PREFIX + "Rates/" + name + ".csv"
+        rates.to_csv(path_or_buf=path_rates,header=True)
+
+
+    #--------------------------------------------------------------------------
+            
     @staticmethod
     def save_attribute(symbol,attributes):
         """
-        Save attributes for a given stock in the Attributes folder
         """
-        
         path_attr = DataLoader.PATH_PREFIX + "Attributes/" + symbol
         pickle.dump(attributes,open(path_attr,"w"))
-    
 
-        
+    #--------------------------------------------------------------------------
+
     @staticmethod
     def save_symbol(symbol=None,symbols=None):
         """
         Save a single symbol or a list/set/tuple of symbols in the Symbols file
-        (where there dumped as a set)
+        (where they are stored as a set object)
         """
         
         s = set(DataLoader.symbols());
@@ -127,105 +192,54 @@ class DataLoader:
         if (symbols is not None) or (symbol is not None):
             pickle.dump(s,open(DataLoader.PATH_PREFIX+"Symbols","w"))
             
-                
-
-
-    @staticmethod
-    def save_stock_data(symbol,erase=False,prices=True,attributes=True):
-        
-        # Look for the name among the symbols already fetched and does nothing
-        # if already retrieved
-        symbols = DataLoader.symbols();
-        
-        if not erase:
-            if symbol in symbols:
-                return
-      
-        # If new symbol:
-        
-        if prices:
-            # Fetch and save historical prices
-            DataLoader.get_historical_prices(symbol,save=True)
-        
-        if attributes:
-            # Fetch attributes and save it in the Attributes folder
-            DataLoader.get_attributes(symbol,save=True)
-        
-        # Add the symbol to the list in the Symbols file
-        if prices or attributes:
-            DataLoader.save_symbol(symbols=symbols,symbol=symbol)
+    ###########################################################################
+    # Functions to read data that has already been saved
+    ###########################################################################
             
-        
-        
     @staticmethod
-    def save_listOf_stocks(path,erase=False,prices=True,attributes=True):
-        
-        # List of the symbols to fetch from the web
-        symbols = DataLoader.read_symbols_from_file(path)
-        # To store the symbols we fail to fetch from the web        
-        errors_list = {}
-        
-        for symbol in symbols:
-            print(symbol)
+    def symbols():
+        """
+        Return the list of symbols already retrieved
+        """
+        if os.path.isfile(DataLoader.PATH_PREFIX+"Symbols"):
+            symbols = pickle.load(open(DataLoader.PATH_PREFIX+"Symbols","r"))
+        else:
+            symbols = set()  
             
-            try:
-                DataLoader.save_stock_data(symbol,erase=erase,prices=prices,attributes=attributes)
-                print("--Successfully saved")
-            except Exception as e:
-                print("-------------------------------")
-                print("--Error occured:" + e.message)
-                print("-------------------------------")                    
-                
-                errors_list[symbol] = e.message
-                
-        return errors_list
-        
-        
-        
+        return symbols        
+            
+    #--------------------------------------------------------------------------
+
     @staticmethod
-    def load_prices(symbol):
+    def read_symbols_from_file(path):
+        """
+        Read symbols stored in a file such that there is one symbole per line
+        """        
+        symbols = []
+        
+        with open(path,"r") as sym_file:
+            for symbol in iter(sym_file):
+                symbols.append(symbol[:-1]) # Last character is the newline '\n'
+                
+        return symbols
+        
+    #--------------------------------------------------------------------------
+
+    @staticmethod
+    def read_prices_attributes(symbol):
         
         if not symbol in DataLoader.symbols():
-            warnings.warn("Symbol not found. No price retrieved")
-            return None
-    
-        return pd.read_csv(DataLoader.PATH_PREFIX+"Prices/"+symbol+".csv",index_col=0,parse_dates=True,header=0,squeeze=True)
-    
+            warnings.warn("Symbol not found in list of symbols. ")
+        
+        prices = pd.read_csv(DataLoader.PATH_PREFIX+"Prices/"+symbol+".csv",index_col=0,parse_dates=True,header=0,squeeze=True)
+        attributes =  pickle.load(open(DataLoader.PATH_PREFIX+"Attributes/"+symbol,"r"))       
+            
+        return prices,attributes
+        
+    #--------------------------------------------------------------------------
 
     @staticmethod
-    def load_attributes(symbol):
-        
-        if not symbol in DataLoader.symbols():
-            warnings.warn("Symbol not found. Attributes not retrieved")
-            return None
-                                          
-        return pickle.load(open(DataLoader.PATH_PREFIX+"Attributes/"+symbol,"r"))
-        
-    
-    @staticmethod
-    def load_prices_attributes(symbol):
-        # Return prices,attributes
-        return DataLoader.load_prices(symbol),DataLoader.load_attributes(symbol)
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
+    def read_index_components(index_name):
 
-     
+        return DataLoader.read_symbols_from_file(DataLoader.PATH_PREFIX + "Lists of symbols/" + index_name)
+    
--- risk_free_rate.py
+++ risk_free_rate.py
@@ -14,41 +14,40 @@ class RiskFreeRate:
     
     def get_first_date(self):
         """
-        Return the starting date for the time series of rates as a string
+        Return the starting date 
         """
         first_date = self.risk_free_rate[0:1].index[0].date()
         first_date = first_date.isoformat()[0:10]
         
         return first_date
         
+        
+    def get_last_date(self):
+        """
+        Return the last date 
+        """
+        last_date = self.risk_free_rate[-1:].index[0].date()
+        last_date = last_date.isoformat()[0:10]
+        
+        return last_date
+        
 
     
-    #--------------------------------------------------------------------------
+    ###########################################################################
     # Predefined risk free rates
-    #--------------------------------------------------------------------------
+    ###########################################################################
     
     @staticmethod
-    def create_fed_funds_rate():
-        risk_free_rate = pd.read_csv(DataLoader.PATH_PREFIX+"Rates/Fed Funds Rate.csv",index_col=0,parse_dates=True,header=0,squeeze=True)
-        return RiskFreeRate(risk_free_rate=risk_free_rate,currency="USD")
+    def create_rates(name):
+
+        currency = {"LIBOR ON USD":"USD",
+                   "LIBOR ON EUR":"EUR",
+                   "LIBOR ON GBP":"GBP",
+                   "FED FUNDS RATES":"USD"}   
+                            
+        risk_free_rate = pd.read_csv(DataLoader.PATH_PREFIX+"Rates/"+name+".csv",index_col=0,parse_dates=True,header=0,squeeze=True)
         
-    @staticmethod
-    def create_LIBOR_overnight_USD():
-        risk_free_rate = pd.read_csv(DataLoader.PATH_PREFIX+"Rates/LIBOR Overnight USD.csv",index_col=0,parse_dates=True,header=0,squeeze=True)
-        return RiskFreeRate(risk_free_rate=risk_free_rate,currency="USD")
-        
-    @staticmethod
-    def create_sonia_rate():
-        risk_free_rate = pd.read_csv(DataLoader.PATH_PREFIX+"Rates/SONIA lending.csv",index_col=0,parse_dates=True,header=0,squeeze=True)
-        return RiskFreeRate(risk_free_rate=risk_free_rate,currency="GBp")
-        
-    @staticmethod
-    def create_eonia_rate():
-        risk_free_rate = pd.read_csv(DataLoader.PATH_PREFIX+"Rates/EONIA lending.csv",index_col=0,parse_dates=True,header=0,squeeze=True)
-        return RiskFreeRate(risk_free_rate=risk_free_rate,currency="EUR")
-        
-        
-        
+        return RiskFreeRate(risk_free_rate=risk_free_rate,currency=currency[name])
         
         
         
