fefcf6e6ed02bd2ae75527b6508fcd9f7d87dacc
jonathanVisbecq <j.visbecq@orange.fr>
2015-02-15 17:08:04 +0100

    MODIF

--- global_data.py
+++ global_data.py
@@ -4,7 +4,8 @@ index_currency = {"SP500":"USD",
                   "FTSE100":"GBP",
                   "EURSTOXX50":"EUR",
                   "DAX30":"EUR",
-                  "CAC40":"EUR"} 
+                  "CAC40":"EUR",
+                  "DJIA":"USD"} 
                   
 interest_rates_currency = {"LIBOR ON USD":"USD",
                            "LIBOR ON EUR":"EUR",
